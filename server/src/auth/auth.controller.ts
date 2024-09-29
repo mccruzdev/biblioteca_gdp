e3fd3fba01a2f19a89692c205d53dbcaaf73c363
angelchavezinformatica <achavezg@unitru.edu.pe>
2024-09-29 14:08:11 -0500

    server- Login users and return JWT

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { LoginUserDTO } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +15,10 @@ export class AuthController {
   @Post('create')
   createUser(@Body() user: CreateUserDTO) {
     return this.auth.createReaderUser(user);
+  }
+
+  @Post('login')
+  loginUser(@Body() user: LoginUserDTO) {
+    return this.auth.loginUser(user);
   }
 }
