3e8c85ad70a2a050cfd4d35e32313163a893b61b
angelchavezinformatica <achavezg@unitru.edu.pe>
2024-12-27 13:38:51 -0500

    fix modal in Users

--- server/scripts/create-superuser.ts
+++ server/scripts/create-superuser.ts
@@ -1,15 +1,44 @@
 import { PrismaClient } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import * as readline from 'readline';
+
+// Función para leer entrada del usuario desde la consola
+function askQuestion(query: string): Promise<string> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question(query, (answer) => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+}
 
 async function createSuperuser() {
   const prisma = new PrismaClient();
 
   try {
-    const email = 'admin@example.com';
-    const password = 'SuperSecurePassword123';
+    // Solicitar datos al usuario
+    const email = await askQuestion('Ingrese el email del superusuario: ');
+    const password = await askQuestion(
+      'Ingrese la contraseña del superusuario: ',
+    );
+    const dni = await askQuestion('Ingrese el DNI del superusuario: ');
+    const names = await askQuestion('Ingrese el nombre(s) del superusuario: ');
+    const lastName = await askQuestion(
+      'Ingrese el apellido del superusuario: ',
+    );
+    const phoneNumber = await askQuestion(
+      'Ingrese el número de teléfono del superusuario: ',
+    );
 
-    // Verifica si ya existe un usuario con este email
-    const existingUser = await prisma.user.findUnique({where: { email },});
+    // Verificar si ya existe un usuario con este email
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
 
     if (existingUser) {
       console.log(`Ya existe un usuario con el email: ${email}`);
@@ -22,10 +51,10 @@ async function createSuperuser() {
     // Crear el superusuario
     const superuser = await prisma.user.create({
       data: {
-        dni: '00000000',
-        names: 'Super',
-        lastName: 'User',
-        phoneNumber: '000000000',
+        dni,
+        names,
+        lastName,
+        phoneNumber,
         password: hashedPassword,
         role: 'ADMIN',
         email,
